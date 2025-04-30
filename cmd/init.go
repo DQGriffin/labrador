a41e453c85e2d97f7c166ec2c5957cde4d471cca
DQ Griffin <dquaviusgriffin@gmail.com>
2025-04-29 21:25:08 -0400

    add customizability to init command

--- cmd/init.go
+++ cmd/init.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"os"
 
+	"github.com/DQGriffin/labrador/internal/cli/console"
 	"github.com/DQGriffin/labrador/pkg/types"
 	"github.com/urfave/cli/v2"
 )
@@ -18,11 +19,39 @@ func InitCommand(flags []cli.Flag) *cli.Command {
 				Usage:   "Environment to initialize project with (ex. dev, staging, prod)",
 				EnvVars: []string{"ENV"},
 			},
+			&cli.StringFlag{
+				Name:    "name",
+				Aliases: []string{"n"},
+				Usage:   "Name of the project",
+				EnvVars: []string{"NAME"},
+			},
+			&cli.StringFlag{
+				Name:    "output",
+				Aliases: []string{"o"},
+				Usage:   "Path to output project config to",
+				EnvVars: []string{"OUTPUT"},
+			},
 		},
 		Action: func(c *cli.Context) error {
+
+			projectName := "my_project"
+			if c.String("name") != "" {
+				projectName = c.String("name")
+			}
+
+			projectEnv := "dev"
+			if c.String("env") != "" {
+				projectEnv = c.String("env")
+			}
+
+			outputPath := "labrador_project.json"
+			if c.String("output") != "" {
+				outputPath = c.String("output")
+			}
+
 			project := types.Project{
-				Name:        "my_project",
-				Environment: c.String("env"),
+				Name:        projectName,
+				Environment: projectEnv,
 				Variables: map[string]string{
 					"version": "1.0",
 				},
@@ -30,15 +59,20 @@ func InitCommand(flags []cli.Flag) *cli.Command {
 			}
 
 			// Marshal the project struct to JSON
-			data, err := json.MarshalIndent(project, "", "  ")
+			data, err := json.MarshalIndent(project, "", "\t")
 			if err != nil {
 				panic(err)
 			}
 
 			// Write the JSON to a file
-			err = os.WriteFile("lab_project.json", data, 0644)
+			err = os.WriteFile(outputPath, data, 0644)
 			if err != nil {
 				panic(err)
+			}
+
+			console.Infof("Wrote project config to %s", outputPath)
+			if c.String("name") == "" && c.String("output") == "" && c.String("env") == "" {
+				console.Info("Tip: use --name, --env, or --output next time to customize")
 			}
 
 			return nil
